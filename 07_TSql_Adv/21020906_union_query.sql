08d82c8cecbff042aeebab648f873f35a1e4bf6c
joohy <joohyun104@gmail.com>
2021-02-09 17:08:30 +0900

    0209 17:08

---
+++ 07_TSql_Adv/21020906_union_query.sql
@@ -0,0 +1,14 @@
+﻿--union
+select ClubName as bName, buildingNum as bLocation from ClubTbl
+union
+select StdName, region from StdTbl;
+
+--union all
+select StdName, region from StdTbl
+union all
+select StdName, region from StdTbl;
+
+--except
+select StdName, region from StdTbl
+union all
+select StdName, region from StdTbl;
