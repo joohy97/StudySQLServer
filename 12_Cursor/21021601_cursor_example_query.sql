a02841fe2b15512eb2c55c162b6f0b89a65ca8d9
joohy <joohyun104@gmail.com>
2021-02-16 10:16:37 +0900

    11장 파일이름 수정, 12장 커서 예제 추가

---
+++ 12_Cursor/21021601_cursor_example_query.sql
@@ -0,0 +1,27 @@
+﻿use sqlDB;
+go
+
+declare cur_usertbl cursor local
+	for select userName, height from userTbl;
+
+open cur_userTbl;
+
+declare @userName nchar(8);
+declare @height smallint;
+declare @cnt int = 0;
+declare @totalHeight int = 0;
+
+fetch next from cur_usertbl into @userName, @height; --커서값 읽어서 @height 변수에 할당
+
+while @@FETCH_STATUS = 0
+begin
+	set @cnt += 1;
+	set @totalHeight += @height;
+	print concat('회원 : ', @userName, '키 : ', @height);
+	fetch next from cur_usertbl into @userName, @height;
+	--print @@fetch_status;
+end
+
+print @totalHeight;
+print @cnt;
+print concat('회원 키 평균 : ', @totalHeight / @cnt);
