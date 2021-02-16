255c5bb6de12b322ab17036c1fdb9d6c77bd8df9
joohy <joohyun104@gmail.com>
2021-02-16 15:27:27 +0900

    0216 15:27

---
+++ 15_XML/21021605_xml_test_query.sql
@@ -0,0 +1,6 @@
+﻿insert into xmlTbl values ('Hello Wolrd');
+
+select * from xmlTbl;
+
+insert into xmlTbl values ('<html><body><p>Sample HTML</p></body></html>');
+insert into xmlTbl values ('<html><body><p>Sample HTML</body></html>');
---
+++ 15_XML/21021606_table_join_xml_test_query.sql
@@ -0,0 +1,6 @@
+﻿select u.userID, u.userName, u.birthYear, u.addr, u.height, b.prodName, b.price
+from userTbl as u
+inner join buyTbl as b
+on u.userID = b.userID
+where u.height > 170
+for xml auto;
